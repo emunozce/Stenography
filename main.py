60d02ae0448211751f7ca5c0ce9e5af7643179c7
Emmanuel Munoz <emunozce80@gmail.com>
2024-05-27 23:22:47 -0600

    chore: improve main.py with file existence check and error handling

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+"""
+
+"""
+
+import sys
+from pathlib import Path
+
 if __name__ == "__main__":
-    print("Hello World!")
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: python main.py <file_path>")
+        sys.exit(1)
+    else:
+        path = Path(args[1])
+        if path.is_dir():
+            print("Usage: python main.py <file_path>, path is not a directory")
+            sys.exit(1)
+        else:
+            if not path.exists():
+                print("File does not exist")
+            else:
+                print("File exists")
